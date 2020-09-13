9e0b0bb07369b351e0be113992f0637faa865d1d
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-13 01:02:47 -0500

    Added the LFTP no symlinks option.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 # TODO: Add list of excluded delete files in two formats, string separated by space and file.
 
 FTP_SETTINGS="set ftp:ssl-allow ${INPUT_SSL_ALLOW}; set ftp:use-feat ${INPUT_USE_FEAT};"
-MIRROR_COMMAND="mirror --continue --reverse --dereference"
+MIRROR_COMMAND="mirror --continue --reverse --no-symlinks"
 
 echo "=== Environment variables ==="
 echo "INPUT_SERVER: ${INPUT_SERVER}"
@@ -14,7 +14,7 @@ echo "INPUT_DELETE: ${INPUT_DELETE}"
 echo "INPUT_LOCAL_DIR: ${INPUT_LOCAL_DIR}"
 echo "INPUT_REMOTE_DIR: ${INPUT_REMOTE_DIR}"
 echo ""
-echo "=== Current location==="
+echo "=== Current location ==="
 pwd
 echo ""
 echo "=== List this directory ==="
@@ -25,9 +25,6 @@ if [ -z "${INPUT_LOCAL_DIR}" ]; then
   INPUT_LOCAL_DIR="./"
 else
   INPUT_LOCAL_DIR="./${INPUT_LOCAL_DIR}/"
-  echo "=== List local directory ==="
-  ls -lha "${INPUT_LOCAL_DIR}"
-  echo ""
 fi
 
 if [ -z "${INPUT_REMOTE_DIR}" ]; then
@@ -39,6 +36,17 @@ fi
 if [ "${INPUT_DELETE}" = "true" ]; then
   MIRROR_COMMAND="${MIRROR_COMMAND} --delete"
 fi
+
+echo "=== Directories ==="
+echo "INPUT_LOCAL_DIR: ${INPUT_LOCAL_DIR}"
+echo "INPUT_REMOTE_DIR: ${INPUT_REMOTE_DIR}"
+echo ""
+echo "=== List local directory | ${INPUT_LOCAL_DIR} ==="
+ls -lha "${INPUT_LOCAL_DIR}"
+echo ""
+echo "=== LFTP ==="
+echo "lftp -> ${FTP_SETTINGS} ${MIRROR_COMMAND} ${INPUT_LOCAL_DIR} ${INPUT_REMOTE_DIR}"
+echo ""
 
 lftp \
   -u "${INPUT_USER}","${INPUT_PASSWORD}" \
